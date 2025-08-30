68daa618a4ad7cca8e169030692a46fb3712fae6
banggyeonghwa <juam0630@gmail.com>
2025-08-30 17:04:09 +0900

    로그인페이지.js

---
+++ 로그인페이지.js
@@ -0,0 +1,22 @@
+ const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const loginBtn = document.getElementById("loginBtn");
+
+    function checkInput() {
+      if (username.value.trim() && password.value.trim()) {
+        loginBtn.classList.add("active");
+        loginBtn.removeAttribute("disabled");
+      } else {
+        loginBtn.classList.remove("active");
+        loginBtn.setAttribute("disabled", true);
+      }
+    }
+
+    username.addEventListener("input", checkInput);
+    password.addEventListener("input", checkInput);
+
+    loginBtn.addEventListener("click", () => {
+      if (loginBtn.classList.contains("active")) {
+        alert("로그인 시도: " + username.value);
+      }
+    });
